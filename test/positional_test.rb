21e8998e39385a5cfd90ef597ccdbd760227baa7
Justin Sharps <jlsharps@alum.mit.edu>
2007-08-14 23:53:38 +0000

    Fixed positional test-case to test for correct winner, not most recent winner.
    git-svn-id: svn://rubyforge.org/var/svn/rubyvote/trunk@32 1440c7f4-e209-0410-9a04-881b5eb134a8

--- test/positional_test.rb
+++ test/positional_test.rb
@@ -8,11 +8,12 @@ class TestPositionalVote < Test::Unit::TestCase
 
   def test_borda
     vote_array = Array.new
+    2.times {vote_array << "BAC".split("")}
     3.times {vote_array << "ABC".split("")}
     3.times {vote_array << "CBA".split("")}
-    2.times {vote_array << "BAC".split("")}
-
+    
     assert_equal( "B", BordaVote.new(vote_array).result.winners[0] )
   end
+  
 end
 
